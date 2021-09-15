c3cadedf7f6dc47f946bc194469941b5839cd899
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-16 00:49:39 +0900

    Feat : 레이아웃의 설정한 각종 View 들 프로퍼티 설정 및 Firebase auth 인스턴스 설정

--- app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.yoochangwonspro.tinderproject
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity: AppCompatActivity() {
 
@@ -12,6 +16,12 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // FirebaseAuth.getInstance() 와 같고 ktx 형식으로 가져오는 방법
+        auth = Firebase.auth
 
+        val emilEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
     }
 }
