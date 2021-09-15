d9903dc6784cdbabc853d007b2e059cc8c295385
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-16 00:45:18 +0900

    Feat : FirebaseAuth 타입의 전역 변수 추가

--- app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.yoochangwonspro.tinderproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
