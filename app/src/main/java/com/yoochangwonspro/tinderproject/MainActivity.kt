b5decaab6d31e89b12a930d93c70d2f1f85a573e
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-16 20:50:36 +0900

    Feat : MainActivity FirebaseAuth 전역 프로퍼티 설정

--- app/src/main/java/com/yoochangwonspro/tinderproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/MainActivity.kt
@@ -2,8 +2,12 @@ package com.yoochangwonspro.tinderproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
