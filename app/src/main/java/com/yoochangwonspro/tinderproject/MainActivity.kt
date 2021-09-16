7f0f572e527db6249cab6716a14865d84a83647d
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-16 23:58:47 +0900

    Feat : MainActivity onStart 함수 구현

--- app/src/main/java/com/yoochangwonspro/tinderproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yoochangwonspro.tinderproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
