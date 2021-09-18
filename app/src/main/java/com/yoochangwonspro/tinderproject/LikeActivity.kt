909b597ee3a5e591c62bb78e3fb92abf349fa55c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-18 19:14:52 +0900

    Feat : LikeActivity 의 FirebaseAuth 전역 변수 설정

--- app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
@@ -2,8 +2,12 @@ package com.yoochangwonspro.tinderproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LikeActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_like)
