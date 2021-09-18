11706489a6eac42d312d7791572df3f86a2bd4fc
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-18 19:16:38 +0900

    Feat : LikeActivity 의 DatabaseReference 전역 변수 선언 및 초기화

--- app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
@@ -3,13 +3,19 @@ package com.yoochangwonspro.tinderproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class LikeActivity : AppCompatActivity() {
 
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    lateinit var userDB: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_like)
+
+        userDB = Firebase.database.reference.child("Users")
     }
 }
