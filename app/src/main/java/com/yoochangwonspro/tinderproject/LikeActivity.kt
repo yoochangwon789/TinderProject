22b1defe779a93d04528992142353d7df4ed16c4
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-18 19:18:15 +0900

    Feat : LikeActivity 의 getCurrentUserID 함수 구현

--- app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
@@ -2,6 +2,7 @@ package com.yoochangwonspro.tinderproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -17,5 +18,14 @@ class LikeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_like)
 
         userDB = Firebase.database.reference.child("Users")
+    }
+
+    private fun getCurrentUserID(): String {
+        if (auth.currentUser == null) {
+            Toast.makeText(this, "로그인이 되어있지 않습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        return auth.currentUser?.uid.orEmpty()
     }
 }
