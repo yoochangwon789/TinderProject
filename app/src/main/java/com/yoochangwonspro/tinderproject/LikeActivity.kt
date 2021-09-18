136dd53b6a03393364fe2b90b1cb0949a033d4e3
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-18 20:23:46 +0900

    Feat : LikeActivity 의 addListenerForSingleValueEvent 메서드 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -18,6 +21,20 @@ class LikeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_like)
 
         userDB = Firebase.database.reference.child("Users")
+
+        val currentUserDB = userDB.child(getCurrentUserID())
+        currentUserDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child("name").value == null) {
+
+                    return
+                }
+
+                // 유저 정보를 갱신해라
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
     private fun getCurrentUserID(): String {
