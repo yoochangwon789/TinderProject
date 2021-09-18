96d4c5d3325f324b1485e146ed86cd99dba7aa68
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-18 17:50:51 +0900

    Feat : initFacebookLoginButton 함수 구현

--- app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LoginActivity.kt
@@ -11,11 +11,16 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var callbackManager: CallbackManager
@@ -46,7 +51,9 @@ class LoginActivity: AppCompatActivity() {
                     if (task.isSuccessful) {
                         finish()
                     } else {
-                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해 주세요.", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this,
+                            "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해 주세요.",
+                            Toast.LENGTH_LONG).show()
                     }
                 }
         }
@@ -62,9 +69,12 @@ class LoginActivity: AppCompatActivity() {
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인 해주세요.", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this,
+                            "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인 해주세요.",
+                            Toast.LENGTH_LONG).show()
                     } else {
-                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
         }
@@ -90,7 +100,34 @@ class LoginActivity: AppCompatActivity() {
     }
 
     private fun initFacebookLoginButton() {
+        val facebookLoginButton = findViewById<LoginButton>(R.id.facebookLoginButton)
 
+        facebookLoginButton.setPermissions("email", "public_profile")
+        facebookLoginButton.registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    // 로그인 성공
+                    val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener(this@LoginActivity) { task ->
+                            if (task.isSuccessful) {
+                                finish()
+                            } else {
+                                Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+                    Toast.makeText(this@LoginActivity, "페이스북 로그인이 실패했습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
     }
 
     private fun getInputEmail(): String {
