dce14fe4eb5041ed9db840d7319438d71369c9b7
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-20 20:36:58 +0900

    Feat : MatchedUserActivity DB 프로퍼티 생성

--- app/src/main/java/com/yoochangwonspro/tinderproject/MatchedUserActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/MatchedUserActivity.kt
@@ -2,8 +2,14 @@ package com.yoochangwonspro.tinderproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class MatchedUserActivity : AppCompatActivity() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    lateinit var userDB: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matched_user)
