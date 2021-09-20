a92c488d34b72c863ea44ab9f76823ab59dde6a6
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-20 21:59:56 +0900

    Feat : initLogoutButton, initMatchedListButton 메서드 생성

--- app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
+++ app/src/main/java/com/yoochangwonspro/tinderproject/LikeActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -49,7 +50,17 @@ class LikeActivity : AppCompatActivity(), CardStackListener {
         })
 
         initCardStackView()
+        initLogoutButton()
+        initMatchedListButton()
         Log.d("CardItem", "$cardItems")
+    }
+
+    private fun initLogoutButton() {
+
+    }
+
+    private fun initMatchedListButton() {
+
     }
 
     private fun initCardStackView() {
