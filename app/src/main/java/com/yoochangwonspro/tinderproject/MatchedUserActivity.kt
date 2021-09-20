b1bf100a06080059705b5f4ae39a35aea840ea65
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-20 20:33:15 +0900

    Feat : MatchedUserActivity 매치가 성사된 엑티비티 생성

---
+++ app/src/main/java/com/yoochangwonspro/tinderproject/MatchedUserActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.tinderproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchedUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matched_user)
+    }
+}
